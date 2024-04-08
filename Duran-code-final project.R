a6c4579a9910da20bbda5655b232e203dfb62ed8
Matthew Duran <134881849+mattduran12@users.noreply.github.com>
2024-04-08 13:11:12 -0700

    Add files via upload

--- Duran-code-final project.R
+++ Duran-code-final project.R
@@ -16,11 +16,10 @@ uof.2017.net <- as.network(
 )
 
 
-
 mat.2017 <- as.matrix(uof.2017.net) # coerce network into a matrix
 
-N <- dim( mat.2017 )[1] # number of police officers
-M <- dim( mat.2017 )[2] # number of incidents
+N <- dim( mat.2017 )[1] # number of police officers -- looks good
+M <- dim( mat.2017 )[2] # number of incidents  -- looks good
 
 uof.2017.net <- as.network(
   mat.2017,
@@ -28,10 +27,71 @@ uof.2017.net <- as.network(
 )
 
 
-# assign the attributes to the network
 
-attrs.2017 <- data.raw.2017
 
+<<<<<<< HEAD
+off.attrb.2017.data <- "https://raw.githubusercontent.com/mattduran12/SAND/main/2017-officer-attributes.csv"
+off.attrb.2017 <- read.csv(off.attrb.2017.data, header = TRUE, row.names = NULL)
+
+inc.attrb.2017.data <- "https://raw.githubusercontent.com/mattduran12/SAND/main/2017-incident-attributes.csv"
+inc.attrb.2017 <- read.csv(inc.attrb.2017.data, header = TRUE, row.names = NULL)
+
+actor_ids <- rownames(mat.2017)
+event_ids <- colnames(mat.2017)
+
+actor_gender <- off.attrb.2017$off_gender[match(actor_ids, off.attrb.2017$off_id)]
+set.vertex.attribute(uof.2017.net, "actor_gender", actor_gender)
+
+
+
+# Define colors for males and females
+genders <- c(
+  off.attrb.2017$off_gender[match(actor_ids, off.attrb.2017$off_id)]
+)
+
+vcol <- genders
+vcol[genders == "Male"] <- "green"
+vcol[genders == "Female"] <- "red"
+
+
+# Plot the network with node colors based on gender
+gplot(
+  uof.2017.net,
+  gmode = "twomode",
+  usearrows = FALSE,
+  vertex.col = vcol,
+  vertex.cex = 1.5,
+  label.cex = 1.2,
+  main = "2017"
+)
+
+# Notes
+off.gender <- off.attrb.2017[,2]
+off.race   <- off.attrb.2017[,3]
+
+
+officer_genders <- off.attrb.2017$off_gender[match(actor_ids, off.attrb.2017$off_id)]
+
+# Define colors for males and females
+officer_colors <- ifelse(officer_genders == "Male", "lightblue", "pink")
+
+# Define a color for events
+event_color <- "grey"
+
+# Combine colors for officers and events
+vertex_colors <- c(officer_colors, rep(event_color, length(event_ids)))
+
+# Plot the network with node colors based on gender
+gplot(
+  uof.2017.net,
+  gmode = "twomode",
+  usearrows = FALSE,
+  vertex.col = vertex_colors,
+  vertex.cex = 1.5,
+  label.cex = 1.2,
+  main = "2017"
+)
+=======
 uof.2017.net %v% "Gender" <- attrs.2017[,3]
 uof.2017.net %v% "Race" <- attrs.2017[,4]
 uof.2017.net %v% "Age" <- attrs.2017[,5]
@@ -40,3 +100,4 @@ uof.2017.net %v% "Cohort" <- attrs.2017[,7]
 uof.2017.net %v% "Number of officers involved per incidnet" <- attrs.2017[,8]
 uof.2017.net %v% "UoF count per officer" <- attrs.2017[,9]
 uof.2017.net %v% "Cohort homophily ratio per incident" <- attrs.2017[,10]
+>>>>>>> 292b053bc92fe01a83551d8166ffd5fc658764fb
